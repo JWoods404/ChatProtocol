85422a4a686e99d9f6a62609cbb1d0e11fa59961
Jeremy Woods <nihillasowagan@gmail.com>
2020-11-19 13:22:55 +0100

    Refactoring nach Unterricht

--- ChatMessage.cs
+++ ChatMessage.cs
@@ -3,6 +3,7 @@
     public class ChatMessage : IMessage
     {
         public string Content { get; set; }
+        public string SessionId { get; set; }
         public string Username { get; set; }
 
         public int MessageId
---
+++ ClientsConnectedMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatProtocol
+{
+    public class ClientsConnectedMessage : IMessage
+    {
+        public string Content { get; set; }
+
+        public int MessageId 
+        { 
+            get { return 5; }
+            set { }
+
+
+        }
+    }
+}
