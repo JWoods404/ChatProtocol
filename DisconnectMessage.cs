f49b27845536f96d765d0f0896a317b93561c8f7
Jeremy Woods <nihillasowagan@gmail.com>
2020-11-20 13:52:23 +0100

    Refactoring after lesson

--- DisconnectMessage.cs
+++ DisconnectMessage.cs
@@ -2,8 +2,6 @@
 {
     public class DisconnectMessage : IMessage
     {
-        public string ClientUsername { get; set; }
-        public string ClientPassword { get; set; }
         public string SessionId { get; set; }
         public int MessageId
         {
