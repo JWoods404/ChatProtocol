8dc78d9e7b5b398cce316a0a305025b9417e1ae9
Jeremy Woods <nihillasowagan@gmail.com>
2020-11-20 16:05:50 +0100

    Aufgabe 7 List of all who are online

--- UserCountMessage.cs
+++ UserCountMessage.cs
@@ -1,9 +1,12 @@
-﻿namespace ChatProtocol
+﻿using System.Collections.Generic;
+
+namespace ChatProtocol
 {
     public class UserCountMessage : IMessage
     {
         public int UserCount { get; set; }
         public int UserOnlineCount { get; set; }
+        public List<string> UsersOnline { get; set; }
 
         public int MessageId
         {
