e5f5e5492ba8083ecf03904a0299fee6f2659535
Jeremy Woods <nihillasowagan@gmail.com>
2020-11-24 18:07:11 +0100

    Direct 1-1 Chat

--- DirectChatMessage.cs
+++ DirectChatMessage.cs
@@ -4,17 +4,17 @@ using System.Text;
 
 namespace ChatProtocol
 {
-    class DirectChatMessage : IMessage
+    public class DirectChatMessage : IMessage
     {
         public string Content { get; set; }
         public string SessionId { get; set; }
-        public string Username { get; set; }
+        //public string Username { get; set; }
         public int UserId { get; set; }
         public int ToUserId { get; set; }
 
         public int MessageId
         {
-            get { return 9; }
+            get { return 10; }
             set { }
         }
     }
